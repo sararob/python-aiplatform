b11eeeeb5699ac6e9b52edb584c9685c47a8dd12
Sara Robinson <sararob@google.com>
2022-08-26 10:38:46 -0400

    add display name to template and upate pipeline urls

--- google/cloud/aiplatform/model_evaluation/model_evaluation_job.py
+++ google/cloud/aiplatform/model_evaluation/model_evaluation_job.py
@@ -38,10 +38,10 @@ _LOGGER = base.Logger(__name__)
 # TODO: update this with the final gcs pipeline template urls
 # First 2 are for automl tabular models, the others are for everything else
 _MODEL_EVAL_PIPELINE_TEMPLATES = {
-    "automl_tabular_without_feature_attribution": "gs://vertex-evaluation-templates/20220823_1843/evaluation_automl_tabular_pipeline.json",
-    "automl_tabular_with_feature_attribution": "gs://vertex-evaluation-templates/20220823_1843/evaluation_automl_tabular_feature_attribution_pipeline.json",
-    "other_without_feature_attribution": "gs://vertex-evaluation-templates/20220823_1843/evaluation_pipeline.json",
-    "other_with_feature_attribution": "gs://vertex-evaluation-templates/20220823_1843/evaluation_feature_attribution_pipeline.json",
+    "automl_tabular_without_feature_attribution": "gs://vertex-evaluation-templates/20220824_2243/evaluation_automl_tabular_pipeline.json",
+    "automl_tabular_with_feature_attribution": "gs://vertex-evaluation-templates/20220824_2243/evaluation_automl_tabular_feature_attribution_pipeline.json",
+    "other_without_feature_attribution": "gs://vertex-evaluation-templates/20220824_2243/evaluation_pipeline.json",
+    "other_with_feature_attribution": "gs://vertex-evaluation-templates/20220824_2243/evaluation_feature_attribution_pipeline.json",
 }
 
 _EXPERIMENTAL_EVAL_PIPELINE_TEMPLATES = {
@@ -260,6 +260,7 @@ class _ModelEvaluationJob(pipeline_based_service._VertexAiPipelineBasedService):
             "evaluation_join_keys": key_columns,
             "model_name": model_resource_name,
             "prediction_type": prediction_type,
+            "evaluation_display_name": display_name,
             "project": project or initializer.global_config.project,
             "location": location or initializer.global_config.location,
             "root_dir": pipeline_root,
